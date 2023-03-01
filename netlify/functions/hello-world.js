385f0c81d27aa27b990a65582a7a5faf4e844c86
Johann Carfantan <jojohann22550@gmail.com>
2023-03-01 15:12:04 +0100

    add an env variable

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,6 +1,7 @@
 exports.handler = async () => {
+    const mySecret = process.env.MY_KEY;
     return {
-        statusCode: 200,
-        body: 'hello world!',
+      statusCode: 200,
+      body: `hello world! I have a ${mySecret}`,
     };
-};
+  };
